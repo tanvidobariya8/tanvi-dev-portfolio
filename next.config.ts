72027e480140aefda8b3a2e544d22e8d92391210
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-02-25 05:19:10 +0530

    add base path in next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: "/tanvi-dev-portfolio",
 };
 
 export default nextConfig;
