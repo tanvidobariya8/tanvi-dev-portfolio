c8d540defc191df5133ba5b424f540f562ebbc32
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-02-25 05:44:23 +0530

    Fix Next.js static export

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
