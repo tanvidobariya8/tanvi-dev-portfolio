66c2acc2cbfeeb993335ddf78d0d963fe3b37c55
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-02-25 05:31:35 +0530

    Fix Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
   basePath: "/tanvi-dev-portfolio",
+  assetPrefix: "/tanvi-dev-portfolio/",
 };
 
 export default nextConfig;
