8d02d54f80bc5c31b8f8b98bc8e92d20adf9a9a1
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-02-25 06:22:41 +0530

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"  // ✅ Add this if using Next.js App Router
   ],
   theme: {
     extend: {},
