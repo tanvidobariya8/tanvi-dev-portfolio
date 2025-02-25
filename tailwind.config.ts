7dc7f8961ecc3218b5f8cc4aefbdc4c37b3c3aa1
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-02-25 21:10:54 +0530

    scss file changes not reflated while development

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // If using App Router
   ],
   theme: {
     extend: {},
