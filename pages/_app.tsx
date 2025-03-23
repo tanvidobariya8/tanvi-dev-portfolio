e210558059138bf3249cdedb2ce50a62813ab7aa
tanvidobariya8 <tanvidobariya6@gmail.com>
2025-03-23 10:40:28 +0530

    add header logo and removed unused images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo5.png" /> {/* Specify size */}
+        <title>Tanvi's Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
